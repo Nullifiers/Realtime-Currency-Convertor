a0cf6a3fd1902ce04d01a819210b57053925432c
Ajeet Goyal <ajeetgoyal1997@gmail.com>
2019-06-06 17:07:57 +0530

    minor fix

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -8,8 +8,8 @@
 
 	const INR = new Intl.NumberFormat('en-IN', {style: 'currency', currency: 'INR'});
 
-	const spanStyle = 'background: rgba(220,220,220,0.7); color: #333; padding: 2px 5px; border-radius: 3px';
-	const format = price => `<span style="${spanStyle}">${INR.format(price)}</span>`
+	const spanStyle = 'background: rgba(220, 220, 220, 0.7); color: #333; padding: 2px 5px; border-radius: 3px';
+	const format = price => `<span style="${spanStyle}">${INR.format(price)}</span>`;
 
 	document.body.innerHTML = document.body.innerHTML.replace(currencyRegex, (_, currency, val) => {
 		const numval = Number(val.replace(/,/g, ''));
