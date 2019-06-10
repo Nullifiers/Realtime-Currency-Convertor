3f78af4f64aea2312a3a59391929a39913b01de5
Ajeet Goyal <ajeetgoyal1997@gmail.com>
2019-06-10 15:28:34 +0530

    run BFS on body & replace only those elements which match the regex

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -1,4 +1,27 @@
 (function() {
+	function replace($node, regex, replaceMethod) {
+		const queue = [$node];
+		while (queue.length) {
+			let top = queue.shift();
+			if (top.childNodes.length) {
+				queue.push(...top.childNodes);
+				continue;
+			}
+
+			// If a text node is only child then replace currency using innerHTML
+			if (top.nodeType === Node.TEXT_NODE && top.parentElement.childNodes.length === 1) {
+				top = top.parentElement;
+			}
+
+			if (top.nodeType === Node.ELEMENT_NODE && regex.test(top.innerHTML)) {
+				top.innerHTML = top.innerHTML.replace(regex, replaceMethod.bind(top));
+			}
+			else if (top.nodeType === Node.TEXT_NODE && regex.test(top.nodeValue)) {
+				top.nodeValue = top.nodeValue.replace(regex, replaceMethod.bind(top));
+			}
+		}
+	}
+
 	const toRupeeRatio = {
 		'$': 69.58,
 		'€': 77.82,
@@ -11,10 +34,12 @@
 	const spanStyle = 'background: rgba(220, 220, 220, 0.7); color: #333; padding: 2px 5px; border-radius: 3px';
 	const format = price => `<span style="${spanStyle}">${INR.format(price)}</span>`;
 
-	document.body.innerHTML = document.body.innerHTML.replace(currencyRegex, (_, currency, val) => {
+	replace(document.body, currencyRegex, function (_, currency, val) {
 		const numval = Number(val.replace(/,/g, ''));
-		return Number.isNaN(numval) ?
-			`${currency} ${val}` :
-			format(toRupeeRatio[currency] * numval);
+		if (Number.isNaN(numval)) return `${currency} ${val}`;
+
+		return this.nodeType === Node.ELEMENT_NODE ?
+			format(toRupeeRatio[currency] * numval) :
+			INR.format(toRupeeRatio[currency] * numval);
 	});
 })();
