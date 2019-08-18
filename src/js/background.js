8eccc768476faac698e16d22fe97ce9c283e590a
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-08-18 23:27:09 +0530

    Add listener for keyboard shortcut

--- src/js/background.js
+++ src/js/background.js
@@ -5,3 +5,18 @@ chrome.browserAction.onClicked.addListener((tab) => {
 		file: 'js/contentScript.js',
 	});
 });
+
+chrome.commands.onCommand.addListener(function (command) {
+	if (command === "convert_currency") {
+		chrome.tabs.query({
+			active: true,
+			currentWindow: true
+		}, function (tabs) {
+			chrome.tabs.executeScript(tabs[0].id, {
+				runAt: 'document_end',
+				allFrames: true,
+				file: 'js/contentScript.js',
+			});
+		});
+	}
+});
